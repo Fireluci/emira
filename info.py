8344646618c07128b8dd2e0cd8be034a50d011a1
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-01-24 11:24:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ MELCOW_PIC = environ.get("MELCOW_PIC", "https://te.legra.ph/file/7bece5ddc3e0018
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/2d68d2c0fbf9385d48d70.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1058015838 640617767').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1058015838 6178075056 640617767').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524 -1002246209699').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '640617767').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
